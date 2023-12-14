97c4605810e8e7cceae22c2bb6ec12179cdf905c
Sanskruti-thorat <tsanskruti@trellissoft.ai>
2023-12-14 16:20:45 +0530

    loaderbootstraP

--- news/src/Routes/LazyLoader.tsx
+++ news/src/Routes/LazyLoader.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
+import { Spinner } from "react-bootstrap";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const LazyLoader =(Component: any) => (props: any) =>
    (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div><Spinner animation="border" /></div>}>
       <Component {...props} />
     </Suspense>
   );
